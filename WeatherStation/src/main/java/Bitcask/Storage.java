8a178830616780b5f5d161fe73e1a99ebdb625dd
usefSaeed <youssef.okab@gmail.com>
2023-05-26 21:59:47 +0300

    hashmap traversal and parameter passing to writing functions done

--- WeatherStation/src/main/java/Bitcask/Storage.java
+++ WeatherStation/src/main/java/Bitcask/Storage.java
@@ -1,8 +1,8 @@
 package Bitcask;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Storage {
     private final static int MAX_FILE_BYTES = 50;
@@ -34,6 +34,10 @@ public class Storage {
         return "storage/" + fileId;
     }
 
+    private String getFilePath(String fileName) {
+        return "storage/" + fileName;
+    }
+
     public EntryPointer write(Entry entry) throws IOException {
         if (activeFile.getFilePointer() + entry.size() > MAX_FILE_BYTES)
             openNewFile();
@@ -61,7 +65,7 @@ public class Storage {
 
     private void compact() throws IOException {
         long filesCount = fileId;
-        HashMap<String,CompactValueNode> compactBlock = new HashMap<>();
+        HashMap<String,CompactValueNode> compactData = new HashMap<>();
         int currentId = hasCompactedBefore ? 0 : 1;
         while (currentId<=filesCount){
             String filePath = getFilePath(currentId++);
@@ -70,17 +74,37 @@ public class Storage {
                 long offset = file.getFilePointer();
                 Entry e = new Entry(file);
                 EntryPointer ep = new EntryPointer(COMPACT_FILE_ID,offset,e.size());
-                if (compactBlock.containsKey(e.getKey())){
-                    compactBlock.replace(e.getKey(),new CompactValueNode(e,ep));
+                if (compactData.containsKey(e.getKey())){
+                    compactData.replace(e.getKey(),new CompactValueNode(e,ep));
                 }else{
-                    compactBlock.put(e.getKey(),new CompactValueNode(e,ep));
+                    compactData.put(e.getKey(),new CompactValueNode(e,ep));
                 }
             }
         }
-        writeCompaction();
+        writeCompactionData(compactData);
     }
 
-    private void writeCompaction() {
-        
+    private void writeCompactionData(HashMap<String,CompactValueNode> compactData) throws FileNotFoundException {
+        String compactFilePath = getFilePath(COMPACT_FILE_ID);
+        RandomAccessFile compactFile = new RandomAccessFile(compactFilePath, "rwd");
+
+        String hintFilePath = getFilePath("hint");
+        RandomAccessFile hintFile = new RandomAccessFile(hintFilePath, "rwd");
+
+        for (Map.Entry<String, CompactValueNode> compactPair : compactData.entrySet()) {
+            String key = compactPair.getKey();
+            CompactValueNode value = compactPair.getValue();
+            writeHintEntry(key,value.getEntryPointer(),hintFile);
+            writeCompactEntry(key,value.getEntry(),compactFile);
+        }
+
     }
+
+    private void writeHintEntry(String key, EntryPointer entryPointer, RandomAccessFile hintFile) {
+    }
+
+    private void writeCompactEntry(String key, Entry entry, RandomAccessFile compactFile) {
+    }
+
+
 }
