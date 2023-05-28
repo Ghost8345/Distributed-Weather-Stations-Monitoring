80e0ea672b8d4343f47ab68e306a5eaeb2e03b27
usefSaeed <youssef.okab@gmail.com>
2023-05-28 11:19:41 +0300

    updated CLI to read from test files in test directory

--- WeatherStation/src/main/java/Bitcask/CLI.java
+++ WeatherStation/src/main/java/Bitcask/CLI.java
@@ -1,47 +1,82 @@
 package Bitcask;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class CLI {
-    public static void main(String[] args) throws IOException, EntryNotFoundException {
-        Bitcask bitcask = new Bitcask();
+    private static Bitcask bitcask;
 
-        Scanner scanner = new Scanner(System.in);
-        String command;
+    public static void main(String[] args) throws IOException, EntryNotFoundException {
+        bitcask = new Bitcask();
+
+        Scanner scanner = null;
 
         System.out.println("Bitcask CLI");
         System.out.println("Available commands: get <key>, put <key> <value>");
 
+
+        File inputFile = new File("test/bitcaskTest1");
+
+        if (inputFile.exists() && inputFile.isFile()) {
+            readFromFile(inputFile);
+        }
+        realTimeRead();
+    }
+
+    private static void readFromFile(File inputFile) throws IOException, EntryNotFoundException {
+        System.out.println("Reading from " + inputFile.getPath());
+        Scanner scanner = new Scanner(inputFile);
+        while (scanner.hasNextLine()) {
+            readOneCommand(scanner);
+        }
+        System.out.println(inputFile.getPath() + " successfully read.");
+        scanner.close();
+    }
+
+    private static void realTimeRead() throws IOException, EntryNotFoundException {
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("> ");
-            command = scanner.nextLine();
-
-            String[] tokens = command.split(" ");
-            if (tokens.length > 0) {
-                String action = tokens[0];
-
-                if (action.equals("get")) {
-                    if (tokens.length == 2) {
-                        String key = tokens[1];
-                        byte[] value = bitcask.get(key);
-                        String valueString = new String(value);
-                        System.out.println(valueString);
-                    } else {
-                        System.out.println("Invalid command. Usage: get <key>");
-                    }
-                } else if (action.equals("put")) {
-                    if (tokens.length == 3) {
-                        String key = tokens[1];
-                        byte[] value = tokens[2].getBytes();
-                        bitcask.put(key, value);
-                    } else {
-                        System.out.println("Invalid command. Usage: put <key> <value>");
-                    }
-                } else {
-                    System.out.println("Invalid command. Available commands: get <key>, put <key> <value>");
-                }
+            boolean isDone = readOneCommand(scanner);
+            if (isDone){
+                System.out.println("Terminated");
+                break;
             }
         }
     }
+
+    private static boolean readOneCommand(Scanner scanner) throws EntryNotFoundException, IOException {
+        String command = scanner.nextLine();
+        if (command.equals("#")){
+            return true;
+        }
+        String[] tokens = command.split(" ");
+        if (tokens.length > 0) {
+            String action = tokens[0];
+
+            if (action.equals("get")) {
+                if (tokens.length == 2) {
+                    String key = tokens[1];
+                    byte[] value = bitcask.get(key);
+                    String valueString = new String(value);
+                    System.out.println(valueString);
+                } else {
+                    System.out.println("Invalid command. Usage: get <key>");
+                }
+            } else if (action.equals("put")) {
+                if (tokens.length == 3) {
+                    String key = tokens[1];
+                    byte[] value = tokens[2].getBytes();
+                    bitcask.put(key, value);
+                } else {
+                    System.out.println("Invalid command. Usage: put <key> <value>");
+                }
+            } else {
+                System.out.println("Invalid command. Available commands: get <key>, put <key> <value>");
+            }
+        }
+        return false;
+    }
+
 }
